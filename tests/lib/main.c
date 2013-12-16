d808493f53e23f26056a3cd8dc4c23a151e1bcb2
Alexandre Devaux <Alexandre.Devaux@student.umons.ac.be>
2013-12-16 10:17:01 +0100

    Small modifications for unit test (main.c).

--- tests/lib/main.c
+++ tests/lib/main.c
@@ -1,6 +1,4 @@
-#include "CUnit/Basic.h"
-
-#define NB_SUITES 3
+#include <CUnit/Basic.h>
 
 int conflict_test_suite(void);
 int structs_test_suite(void);
@@ -11,12 +9,14 @@ int main(void) {
     if (CUE_SUCCESS != CU_initialize_registry())
         return CU_get_error();
 
-    int (* suites_functions[NB_SUITES])(void) = {structs_test_suite,
-                                                 conflict_test_suite,
-                                                 heuristics_test_suite
-                                                };
+    int (* suites_functions[])(void) = {structs_test_suite,
+                                        conflict_test_suite,
+                                        heuristics_test_suite
+                                        };
 
-    for (int i = 0; i < NB_SUITES; i++) {
+    int size = sizeof(suites_functions)/sizeof(suites_functions)[0];
+
+    for (int i = 0; i < size; i++) {
         if (CUE_SUCCESS != suites_functions[i]()) {
             CU_cleanup_registry();
             return CU_get_error();
