b3856cff5f925b8a9766608c69e899ea4670b167
Quentin Loos <contact@quentinloos.be>
2014-02-02 19:24:17 +0100

    Remove main.c and refactor examtimetable.py

--- examtimetable.py
+++ examtimetable.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 
 # Python imports
+import argparse
 import json
 import ctypes
 p = ctypes.POINTER
@@ -9,64 +10,104 @@ p = ctypes.POINTER
 from examtimetable import c_functions, c_structs
 from examtimetable.print_timetable import print_detailed_timetable
 
+DESC = "Generates a feasible examination timetable given examination data"
 
-with open('datasets/example1.json') as f:
-    fs = json.load(f)
 
-# Exams
+def getArgs():
+    parser = argparse.ArgumentParser(description=DESC)
+    parser.add_argument("file")
+    return parser.parse_args()
 
-c_exams = []
 
-for exam in fs['exams']:
-    c_exam = c_functions.init_exam(
-        exam['exam_id'],
-        exam['faculty'],
-        exam['teacher_id'],
-        (ctypes.c_uint32 * len(exam['students']))(*exam['students']),
-        len(exam['students']),
-        exam['room_type'],
-        (ctypes.c_bool * len(exam['availabilities']))(*exam['availabilities']),
-        fs['max_timeslot'],
-        len(fs['exams']),
-        (ctypes.c_uint16 * len(exam['dependencies']))(*exam['dependencies']),
-        len(exam['dependencies']),
-    )
-    c_exams.append(c_exam)
+def get_array_exams(data):
+    '''
+    Creates exam C structure given the data in parameters
+    and returns an array_exams C structure with initialized exams.
+    '''
 
-c_exams_p = ctypes.cast((p(c_structs.Exam) * len(c_exams))(*c_exams),
-                        p(p(c_structs.Exam)))
+    c_exams = []
 
-c_array_exams = c_functions.init_array_exams(len(c_exams), c_exams_p)
+    for exam in data['exams']:
+        c_exam = c_functions.init_exam(
+            exam['exam_id'],
+            exam['faculty'],
+            exam['teacher_id'],
+            (ctypes.c_uint32 * len(exam['students']))(*exam['students']),
+            len(exam['students']),
+            exam['room_type'],
+            (ctypes.c_bool * len(exam['availabilities']))(
+                *exam['availabilities']),
+            data['max_timeslot'],
+            len(data['exams']),
+            (ctypes.c_uint16 * len(exam['dependencies']))(
+                *exam['dependencies']),
+            len(exam['dependencies']),
+        )
+        c_exams.append(c_exam)
 
-# Rooms
+    c_exams_p = ctypes.cast((p(c_structs.Exam) * len(c_exams))(*c_exams),
+                            p(p(c_structs.Exam)))
 
-faculty_size = 0
-c_rooms = []
+    return c_functions.init_array_exams(len(c_exams), c_exams_p)
 
-for room in fs['rooms']:
-    faculty_size = max(faculty_size, room['faculty'])
-    c_room = c_functions.init_room(
-        room['room_id'],
-        room['room_type'],
-        room['capacity'],
-        room['faculty'],
-        fs['max_timeslot']
-    )
-    c_rooms.append(c_room)
 
-faculty_size += 1
+def get_faculty_size(data):
+    '''
+    Returns the number of different faculties in the given data.
+    '''
 
-c_rooms_p = ctypes.cast((p(c_structs.Room) * len(c_rooms))(*c_rooms),
-                        p(p(c_structs.Room)))
+    faculty_size = 0
 
-c_array_rooms  = c_functions.init_array_rooms(len(c_rooms), c_rooms_p)
-c_rooms_sizes  = c_functions.get_rooms_sizes(faculty_size, c_array_rooms)
-c_rooms_matrix = c_functions.get_rooms_matrix(faculty_size, c_array_rooms,
-                                              c_rooms_sizes)
+    for room in data['rooms']:
+        faculty_size = max(faculty_size, room['faculty'])
 
-# Compute conflicts
-c_functions.preprocess(c_array_exams)
+    return faculty_size + 1
 
-if c_functions.color_graph_backtrack(c_array_exams, c_rooms_matrix,
-                                     faculty_size, fs['max_timeslot']):
-    print_detailed_timetable(c_array_exams, fs['max_timeslot'])
+
+def get_rooms_matrix(data, faculty_size):
+    '''
+    Create room C structure given the data in parameters and
+    returns rooms_matrix C structure.
+    '''
+
+    c_rooms = []
+
+    for room in data['rooms']:
+        c_room = c_functions.init_room(
+            room['room_id'],
+            room['room_type'],
+            room['capacity'],
+            room['faculty'],
+            data['max_timeslot']
+        )
+        c_rooms.append(c_room)
+
+    c_rooms_p = ctypes.cast((p(c_structs.Room) * len(c_rooms))(*c_rooms),
+                            p(p(c_structs.Room)))
+
+    c_array_rooms  = c_functions.init_array_rooms(len(c_rooms), c_rooms_p)
+    c_rooms_sizes  = c_functions.get_rooms_sizes(faculty_size, c_array_rooms)
+    c_rooms_matrix = c_functions.get_rooms_matrix(faculty_size, c_array_rooms,
+                                                  c_rooms_sizes)
+
+    return c_rooms_matrix
+
+
+def main():
+    args = getArgs()
+    with open(args.file) as f:
+        data = json.load(f)
+
+    c_array_exams  = get_array_exams(data)
+    faculty_size   = get_faculty_size(data)
+    c_rooms_matrix = get_rooms_matrix(data, faculty_size)
+
+    c_functions.preprocess(c_array_exams)
+
+    if c_functions.color_graph_backtrack(c_array_exams, c_rooms_matrix,
+                                         faculty_size, data['max_timeslot']):
+        print_detailed_timetable(c_array_exams, data['max_timeslot'])
+
+
+if __name__ == '__main__':
+    main()
